efb3b8ecf1a0fcdc87b067301c8de3969129dd8b
fikiap23 <fikiaprian23@gmail.com>
2025-09-09 06:49:03 +0700

    feat: add type category

--- src/product/dto/category-product.dto.ts
+++ src/product/dto/category-product.dto.ts
@@ -1,11 +1,22 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNotEmpty, IsOptional, IsString, Length } from 'class-validator';
+import { TypeProductService } from '@prisma/client';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
 import { SearchPaginationDto } from 'utils/dto/pagination.dto';
 
 export class CreateCategoryProductDto {
   @IsString()
   @IsNotEmpty()
   name: string;
+
+  @IsEnum(TypeProductService)
+  @IsOptional()
+  type?: TypeProductService;
 }
 
 export class UpdateCategoryProductDto extends PartialType(
