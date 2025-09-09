9c3f151c4d72038ea6e63cb4f7fa7c5723faf6a7
fikiap23 <fikiaprian23@gmail.com>
2025-09-09 12:50:26 +0700

    fix get category

--- src/prisma/queries/product/props/select-category-product.prop.ts
+++ src/prisma/queries/product/props/select-category-product.prop.ts
@@ -1,6 +1,10 @@
 import { Prisma } from '@prisma/client';
 
 export const selectGeneralCategoryProduct: Prisma.CategoryProductSelect = {
+  id: true,
   uuid: true,
   name: true,
+  type: true,
+  createdAt: true,
+  updatedAt: true,
 };
